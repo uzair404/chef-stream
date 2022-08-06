38a19310f3b7540780012366f95ca9fb78741148
uzair404 <uzair3w3@gmail.com>
2022-08-06 23:18:03 +0500

    email verification added without ui

--- chef-stream/routes/web.php
+++ chef-stream/routes/web.php
@@ -20,17 +20,17 @@ Route::get('/', function () {
 
 Route::get('/dashboard/setting', function () {
     return view('chef-dashboard.setting');
-});
+})->middleware(['auth', 'verified']);
 
-Route::get('/dashboard/password', [ChangePassword::class, "index"])->name('passwordpage');
-Route::post('/dashboard/password', [ChangePassword::class, "change"])->name('changepass');
+Route::get('/dashboard/password', [ChangePassword::class, "index"])->name('passwordpage')->middleware(['auth', 'verified']);
+Route::post('/dashboard/password', [ChangePassword::class, "change"])->name('changepass')->middleware(['auth', 'verified']);
 
 Route::get('/dashboard/courses', function () {
     return view('chef-dashboard.courses');
-});
+})->middleware(['auth', 'verified']);
 
 Route::get('/dashboard', function () {
     return view('chef-dashboard.dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
